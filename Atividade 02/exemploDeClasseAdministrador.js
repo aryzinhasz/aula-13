dbd4ad70ac9fdcbda6b1e201ce709bcb63e84dbb
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-17 21:45:23 -0300

    Create exemploDeClasseAdministrador.js

---
+++ Atividade 02/exemploDeClasseAdministrador.js
@@ -0,0 +1,28 @@
+var Administrador = /** @class */ (function () {
+    function Administrador(nome, senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+    // Método privado para alterar senha
+    Administrador.prototype.alterarSenha = function (novaSenha) {
+        this.senha = novaSenha;
+        console.log("Senha alterada com sucesso!");
+    };
+    // Método público para solicitar alteração da senha (simulação)
+    Administrador.prototype.solicitarAlteracaoSenha = function (novaSenha) {
+        this.alterarSenha(novaSenha);
+    };
+    // Método público para exibir dados (só nome, não a senha!)
+    Administrador.prototype.mostrarInfo = function () {
+        console.log("Administrador: ".concat(this.nome));
+    };
+    return Administrador;
+}());
+// Criando objeto
+var admin = new Administrador("Carlos", "1234");
+// Acessando métodos
+admin.mostrarInfo();
+// Tentando acessar diretamente (vai dar erro!)
+// admin.alterarSenha("novaSenha"); ❌ (não permitido, pois é private)
+// Acessando indiretamente pelo método público
+admin.solicitarAlteracaoSenha("novaSenha123");
